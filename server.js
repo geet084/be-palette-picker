721ae6d024652d40dd31ac2bca8cf467fb748e0d
Eric Weissman <eric.weissman21@gmail.com>
2019-03-18 17:00:53 -0600

    Adds get endpoints for specific resources Co-authored-by: Travis Gee <geet084@gmail.com>

--- server.js
+++ server.js
@@ -32,3 +32,35 @@ app.get('/api/v1/palettes', (req, res) => {
     })
 });
 
+app.get('/api/v1/projects/:id', (req, res) => {
+  const { id } = req.params
+
+  database('projects').where('id', id).select()
+    .then(project => {
+      if (project[0]) {
+        res.status(200).json(project[0])
+      } else {
+        res.status(404).json(`Requested project does not exist!`)
+      }
+    })
+    .catch(error => {
+      res.status(500).json(`Please check endpoint - valid params not provided! Error: ${error}`)
+    })
+});
+
+app.get('/api/v1/palettes/:id', (req, res) => {
+  const { id } = req.params
+
+  database('palettes').where('id', id).select()
+    .then(palette => {
+      if (palette[0]) {
+        res.status(200).json(palette[0])
+      } else {
+        res.status(404).json(`Requested palette does not exist!`)
+      }
+    })
+    .catch(error => {
+      res.status(500).json(`Please check endpoint - valid params not provided! Error: ${error}`)
+    })
+});
+
