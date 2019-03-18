92cc36b4ff0413407e411a40fa09ca0d8fa75ccc
Travis Gee <geet084@gmail.com>
2019-03-18 15:10:35 -0600

    Setup database tables

---
+++ db/migrations/20190318145911_initial.js
@@ -0,0 +1,31 @@
+
+exports.up = (knex, Promise) => {
+  return Promise.all([
+    knex.schema.createTable('projects', table => {
+      table.increments('id').primary();
+      table.string('project_name');
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('palettes', table => {
+      table.increments('id').primary();
+      table.string('palette_name');
+      table.string('color_1');
+      table.string('color_2');
+      table.string('color_3');
+      table.string('color_4');
+      table.string('color_5');
+      table.integer('project_id').unsigned()
+      table.foreign('project_id')
+        .references('projects.id');
+      table.timestamps(true, true);
+    })
+  ]);
+};
+
+exports.down = (knex, Promise) => {
+  return Promise.all([
+    knex.schema.dropTable('palettes'),
+    knex.schema.dropTable('projects')
+  ]);
+};
