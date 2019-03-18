6704f6ccf3e301d4669df7ef28c783bb7f347631
Travis Gee <geet084@gmail.com>
2019-03-18 15:53:44 -0600

    Add example data per specs
    Co-authored-by: Eric Weissman <eric.weissman21@gmail.com>

---
+++ data/data.js
@@ -0,0 +1,49 @@
+module.exports = [
+  {
+    "project_name": "front end project",
+    "palettes": [
+      {
+        "palette_name": "blue colors",
+        "color_1": "3a7dff",
+        "color_2": "aaa553",
+        "color_3": "f1f1f1",
+        "color_4": "f0e92c",
+        "color_5": "22ddf9"
+      }
+    ]
+  },
+  {
+    "project_name": "back end project",
+    "palettes": [
+      {
+        "palette_name": "dreamy",
+        "color_1": "99cc32",
+        "color_2": "01df33",
+        "color_3": "ee22ff",
+        "color_4": "76fd3c",
+        "color_5": "7a1b9f"
+      },
+      {
+        "palette_name": "steamy colors",
+        "color_1": "643eed",
+        "color_2": "111eee",
+        "color_3": "d3a2c1",
+        "color_4": "ee21aa",
+        "color_5": "9d519a"
+      }
+    ]
+  },
+  {
+    "project_name": "personal project",
+    "palettes": [
+      {
+        "palette_name": "happiness",
+        "color_1": "88ff88",
+        "color_2": "ffffff",
+        "color_3": "eeeeee",
+        "color_4": "9a1f44",
+        "color_5": "8a3d61"
+      }
+    ]
+  }
+]
