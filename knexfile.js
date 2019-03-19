9d477af103933dca7692fea490305f58d82a7b91
Travis Gee <geet084@gmail.com>
2019-03-19 17:10:31 -0600

    Adds test environment for database
    Co-authored-by: Eric Weissman <eric.weissman21@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,17 @@ module.exports = {
       directory: './db/seeds/development'
     }
   },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/projects_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true,
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
