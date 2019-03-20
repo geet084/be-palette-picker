7afaf83744dd0afd3dad5c67de20de334e76fd67
Travis Gee <geet084@gmail.com>
2019-03-20 14:16:20 -0600

    Add tests for GET /projects/:id/palettes
    Co-authored-by: Eric Weissman <eric.weissman21@gmail.com>

--- app.spec.js
+++ app.spec.js
@@ -6,9 +6,12 @@ const database = require('knex')(configuration);
 const projects = require('./data/data')
 
 describe('Server', () => {
+  beforeEach(async () => {
+    await database.seed.run()
+  })
 
   describe('GET /projects', () => {
-    it('should return a status of 200 if okay', async () => {
+    it('should return a status of 200 if OK', async () => {
       const response = await request(app).get('/api/v1/projects');
 
       expect(response.status).toBe(200)
@@ -21,6 +24,35 @@ describe('Server', () => {
       const results = response.body;
 
       expect(results.length).toEqual(expectedProjects);
+    })
+  })
+
+  describe('GET /projects/:id/palettes', () => {
+    it('should return a status of 200 if OK', async () => {
+      const firstProject = await database('projects').first()
+      const id = firstProject.id
+
+      const response = await request(app).get(`/api/v1/projects/${id}/palettes`)
+
+      expect(response.status).toBe(200);
+    })
+
+    it('should return a status of 404 if not OK', async () => {
+      const id = -1
+      const response = await request(app).get(`/api/v1/projects/${id}/palettes`)
+
+      expect(response.status).toBe(404)
+    })
+
+    it('should return all of the palettes for a project in the DB if the response is OK', async () => {
+      const expectedPalettes = projects[1].palettes.length
+      const foundProject = await database('projects').where('project_name', 'back end project')
+      const id = foundProject[0].id
+
+      const response = await request(app).get(`/api/v1/projects/${id}/palettes`)
+      const results = response.body
+
+      expect(results.length).toEqual(expectedPalettes)
     })
   })
 })
